0f44f8f3274fb3641df4e94a9560f5fa3b5b4899
Russell Gordon <rgordon@lcs.on.ca>
2021-10-18 15:24:49 -0400

    Initial Commit

---
+++ ButtonStateExample/ButtonStateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStateExampleApp.swift
+//  ButtonStateExample
+//
+//  Created by Russell Gordon on 2021-10-18.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
